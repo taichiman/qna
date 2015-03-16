de953f6fa13c526fb88c832235dd847deef1e43b
Denis Krivostchokov <densomart@gmail.com>
2015-03-16 15:54:37 +0600

    add foreign index to answers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312173159) do
+ActiveRecord::Schema.define(version: 20150316094527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20150312173159) do
     t.text    "body"
     t.integer "question_id"
   end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
