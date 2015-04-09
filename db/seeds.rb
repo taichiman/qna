074ba566eb1d8c5337c297653cd9d68d72c4b8f8
Denis Krivostchokov <densomart@gmail.com>
2015-04-09 22:42:58 +0600

    add development db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+FactoryGirl.create_list :question_with_answers, 4
