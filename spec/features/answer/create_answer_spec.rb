5140c151c47eb8065d6f92c7f897cf035156ce74
Denis Krivostchokov <densomart@gmail.com>
2015-04-10 23:49:24 +0600

    add scenario: user can select question

---
+++ spec/features/answer/create_answer_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+feature 'User can create answers', %q{
+  In order to be able to answer
+  As an User
+  I want to be able to fill and submit its form
+} do
+
+  given(:question){ Question.first }
+
+  background :all do
+    create_pair :question_with_answers
+  end
+
+  scenario 'An user select the question from question list' do
+    visit '/'
+    click_on question.title
+    expect(page).to have_selector('a.question-hyperlink', text: question.title, count: 1)
+    expect(page).to have_content(question.body)
+  end
+
+  scenario 'An user creates an answer' do
+    visit question_path(question)
+
+    #
+    # click_on 'Create Answer'
+    #
+    # have_content('form new_answer')
+    #
+    # fill_in #question body
+    #
+    # click_on 'Post Your Answer'
+  end
+
+  scenario 'An user can\'t creates an invalid answer' do
+    # given(## answer with blank body)
+  end
+
+end
