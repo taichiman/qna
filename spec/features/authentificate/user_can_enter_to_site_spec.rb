7099dd3ac774d55c8990288a28ce6081e8dd11d6
Denis Krivostchokov <densomart@gmail.com>
2015-04-17 13:41:42 +0600

    ref: feature user can enter to site

--- spec/features/authentificate/user_can_enter_to_site_spec.rb
+++ spec/features/authentificate/user_can_enter_to_site_spec.rb
@@ -1,39 +1,33 @@
 require 'rails_helper'
 
 feature 'User can login to the site', %q{
-  In order to be able to login to the site
+  In order to be able to ack question
   As an User
-  I want to fill Login form
-  Then I will be logged to site
+  I want to be able to signin
 } do
 
-  scenario 'fill form with valid password' do
-    User.create(email:'user@test.com', password: 123)
+  background do
     visit new_user_session_path
-
+  end
+  
+  scenario 'registered user try to sign to site' do
+    User.create(email:'user@test.com', password: 123)
     fill_in 'Email', with: 'user@test.com'
     fill_in 'Password', with: '123'
-    click_on 'Log in'
+    click_button 'Log in'
 
     expect(page).to have_content('Signed in successfully')
     expect(current_path).to eq(root_path)
   end
-  scenario 'fill form with invalid password'
 
   scenario 'Non-register user try to sign in' do
-    visit new_user_session_path
-
-    fill_in 'Email', with: 'user1@test.com'
+    fill_in 'Email', with: 'user_1@test.com'
     fill_in 'Password', with: '123'
-    click_on 'Log in'
+    click_button 'Log in'
 
     expect(page).to have_content('Invalid email or password')
     expect(current_path).to eq(new_user_session_path)
-    
   end
 
-  scenario 'Registered user try to signin' do
-
-
-  end
 end
+
