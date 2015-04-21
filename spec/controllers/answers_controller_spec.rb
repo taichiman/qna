864b3c1b013529c2151a1903a446120331df06b6
Denis Krivostchokov <densomart@gmail.com>
2015-04-21 18:55:52 +0600

    ref: answer controller spec #create

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe AnswersController do
+  let(:question) { create :question }
+  
   describe 'GET #new' do
-    let(:question) { create :question }
-
-    context 'authenticated user' do
+    context 'when authenticated user' do
       sign_in_user
       before{ get(:new, question_id: question.id) }
 
@@ -22,64 +22,68 @@ describe AnswersController do
 
     end
 
-    context 'unauthenticated user' do
+    context 'when unauthenticated user' do
       before{ get :new, question_id: question.id }
 
-      it { should set_flash[:alert].to('You need to sign in or sign up before continuing.') }
-      it { should redirect_to(new_user_session_path) }
+      check_set_alert_flash_and_redirect_to?
    end
 
   end
 
   describe 'POST #create' do
-    context 'with valid attributes' do
-      it 'assigns the question to @question' do
-        question = create(:question)
-        post :create,
+    context 'when authentificated user' do
+      sign_in_user
+
+      context 'with valid attributes' do
+        before { post :create,
           question_id: question.id,
-          answer: attributes_for(:answer)
+          answer: attributes_for(:answer) 
+        }
 
-        expect(assigns (:question)).to eql(question)
+        it 'assigns the question to @question' do
+          expect(assigns (:question)).to eql(question)
+        end
+
+        it 'creates an answer' do
+          expect{ post :create,
+                    question_id: question.id,
+                    answer: attributes_for(:answer)
+
+          }.to change(question.answers, :count).by(1)
+        end
+
+        it 'redirects to show answer' do
+          should redirect_to(question_path(question))
+        end
+
       end
 
-      it 'creates an answer' do
-        question = create :question
-        expect{ post :create,
-                  question_id: question.id,
-                  answer: attributes_for(:answer)
+      context 'with invalid attributes' do
+        before { post :create,
+            question_id: question.id,
+            answer: attributes_for(:invalid_answer)
+        }
 
-        }.to change(question.answers, :count).by(1)
+        it 'doesn\'t create answer' do
+          expect{ post :create,
+                    question_id: question.id,
+                    answer: attributes_for(:invalid_answer)
+          }.not_to change(Answer, :count)
+        end
+
+        it { should render_template(:new) }
       end
 
-      it 'redirects to show answer' do
-        question = create(:question)
-        post :create,
-          question_id: question.id,
-          answer: attributes_for(:answer)
-
-        should redirect_to(question_path(question))
-      end
     end
 
-    context 'with invalid attributes' do
-      let(:question){ create(:question) }
-
-      it 'does not create answer' do
-        expect{ post :create,
-                  question_id: question.id,
-                  answer: attributes_for(:invalid_answer)
-
-        }.not_to change(Answer, :count)
-      end
-
-      it 're-renders the new view' do
-        post :create,
-          question_id: question.id,
-          answer: attributes_for(:answer, body: nil)
-
-        expect(response).to render_template(:new)
-      end
+    context 'when unauthenticated user' do
+      before { post :create,
+        question_id: question.id,
+        answer: attributes_for(:answer) 
+      }
+      check_set_alert_flash_and_redirect_to?
     end
+
   end
 
   describe "GET #show" do
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -6,5 +6,11 @@ module ControllerMacros
       sign_in @user
     end
   end
+
+  def check_set_alert_flash_and_redirect_to?
+    it { should set_flash[:alert].to('You need to sign in or sign up before continuing.') }
+    it { should redirect_to(new_user_session_path) }
+  end
+
 end
 
