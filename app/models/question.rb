5a97ae036e8d69c914dd512b5b7ea36f87572811
Denis Krivostchokov <densomart@gmail.com>
2015-04-27 17:06:34 +0600

    ref: question model :dependency

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  has_many :answers, dependent: :destroy
+  has_many :answers, dependent: :restrict_with_exception
 
   validates :title, :body, presence: true
 
