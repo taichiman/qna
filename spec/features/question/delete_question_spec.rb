119c92db196c1aff6e20911b7a13077c82fa4038
Denis Krivostchokov <densomart@gmail.com>
2015-04-30 20:37:31 +0600

    add: some examples of delete question feature

--- spec/features/question/delete_question_spec.rb
+++ spec/features/question/delete_question_spec.rb
@@ -6,25 +6,40 @@ feature 'User can delete question', %q{
   I want to be able to delete my question
 } do
   
-  feature 'Delete question' do
+  feature 'delete question' do
     feature 'when authenticated user' do
-      given!(:user) { create :user_with_questions }
-      given(:question) { user.questions.first }
-    
-      feature 'deletes owned him questions' do
-        scenario 'should not delete when there are an answers', js: true do 
-          fill_form_and_sign_in(user)
-          visit '/'
-          click_on t('links.my-questions')      
-          link = find("a.delete-question[href='#{question_path(question)}']")
-          link.click
+      background do
+        fill_form_and_sign_in(user)
+        visit '/'
+        click_on t('links.my-questions')      
+        link = find("a.delete-question[href='#{question_path(question)}']")
+        link.click
+      end
 
+      feature 'owned question with an answers' do
+        given(:user) { create :user_with_questions }
+        given(:question) { user.questions.first }
+        
+        scenario 'should not delete', js: true do 
           expect(page).to have_content(t('questions.destroy.not-deleted'))
 
         end
+      end
 
-        scenario 'without answers' do
+      feature 'owned question without an answers' do
+        given(:user) do
+          user = create(:user)
+          user.questions = [
+            create(:question_with_answers, answers_count: 0),
+            create(:question_with_answers)
+          ]
+          user
+        end
+        given(:question) { user.questions.first }
 
+         scenario 'should deletes', js: true do 
+           expect(page).to have_content(t('questions.destroy.deleted'))
+          
         end
       end
 
