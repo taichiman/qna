809972f249a70fadf07bcc32e77ff5619ce0f26e
Denis Krivostchokov <densomart@gmail.com>
2015-07-09 19:36:32 +0600

    Refactoring: Attachment index to compound index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708130026) do
+ActiveRecord::Schema.define(version: 20150709132430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +34,7 @@ ActiveRecord::Schema.define(version: 20150708130026) do
     t.string  "attachable_type"
   end
 
-  add_index "attachments", ["attachable_id"], name: "index_attachments_on_attachable_id", using: :btree
-  add_index "attachments", ["attachable_type"], name: "index_attachments_on_attachable_type", using: :btree
+  add_index "attachments", ["attachable_id", "attachable_type"], name: "index_attachments_on_attachable_id_and_attachable_type", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
