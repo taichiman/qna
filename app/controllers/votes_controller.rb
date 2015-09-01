4fec8699dd699a384a9cfd01cb19ea747cde8259
Denis Krivostchokov <densomart@gmail.com>
2015-09-01 14:49:09 +0600

    add question vote down feature

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -20,8 +20,20 @@ class VotesController < ApplicationController
   end
 
   def vote_down
-    s = { error: t('votes.cancel-previous-vote') }
-    render json: s, status: :unprocessable_entity
+    state = current_user.vote_state_for @votable
+
+    case state
+    when :no_vote
+      current_user.votes.create!(votable: @votable, vote_type: 'down')
+      s = { vote_down: 1 , vote_count: 1 }
+
+    when :up_vote
+      s = { error: t('votes.cancel-previous-vote') }
+      status = :unprocessable_entity
+      
+    end
+
+    render json: s, status: status
 
   end
 
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -7,7 +7,7 @@ describe VotesController do
     context 'when user authenticated' do
       sign_in_user
 
-      describe 'can create vote-up entity' do
+      context 'can create vote-up entity' do
         it 'change Vote count' do
           expect{ xhr :post, :vote_up, id: question.id }
           .to change(Vote,:count)
@@ -19,7 +19,7 @@ describe VotesController do
           xhr :post, :vote_up, id: question.id
 
           expect(question.votes.first)
-          .to have_attributes(votable_id: question.id, votable_type:'Question', user_id: user.id)
+          .to have_attributes(votable_id: question.id, votable_type:'Question', user_id: user.id, vote_type: 'up')
 
         end
       end
@@ -75,5 +75,31 @@ describe VotesController do
     end
 
   end
+
+  describe 'POST#vote-down' do
+    let(:question){ create :question }
+
+    context 'when user authenticated' do
+      sign_in_user
+
+      context 'can create vote-down entity' do
+        it 'change Vote count' do
+          expect{ xhr :post, :vote_down, id: question.id }
+          .to change(Vote,:count)
+          .from(0).to(1)
+   
+        end
+
+        it 'new vote record is the question voting' do
+          xhr :post, :vote_down, id: question.id
+
+          expect(question.votes.first)
+          .to have_attributes(votable_id: question.id, votable_type:'Question', user_id: user.id, vote_type: 'down')
+
+        end
+      end
+    end
+  end
+
 end
 
